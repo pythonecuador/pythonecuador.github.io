ac8c37e37389881d5a9fd9bf23c665072ebe7545
Santos Gallegos <santos_g@outlook.com>
2018-04-15 15:57:33 -0500

    No copiar archivos originales al build final

--- conf.py
+++ conf.py
@@ -1124,10 +1124,10 @@ MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code', 'markdown.extensions.c
 
 # Show link to source for the posts?
 # Formerly known as HIDE_SOURCELINK (inverse)
-# SHOW_SOURCELINK = True
+SHOW_SOURCELINK = False
 # Copy the source files for your pages?
 # Setting it to False implies SHOW_SOURCELINK = False
-# COPY_SOURCES = True
+COPY_SOURCES = False
 
 # Modify the number of Post per Index Page
 # Defaults to 10
