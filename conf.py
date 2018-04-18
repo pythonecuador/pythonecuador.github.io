5dad6ae278c67c5c3d47c3cd702d3963d113ac6d
Santos Gallegos <santos_g@outlook.com>
2018-04-18 11:25:02 -0500

    Agrega tema personalizado

--- conf.py
+++ conf.py
@@ -144,7 +144,7 @@ NAVIGATION_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootstrap3"
+THEME = "custom"
 
 # Primary color of your theme. This will be used to customize your theme and
 # auto-generate related colors in POSTS_SECTION_COLORS. Must be a HEX value.
