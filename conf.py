bae1696c3951afbd3bc055563e3dec0bf277a32c
Santos Gallegos <santos_g@outlook.com>
2018-04-18 14:50:40 -0500

    Agrega archivo de donde iría nuestra página inicial

--- conf.py
+++ conf.py
@@ -552,7 +552,7 @@ HIDDEN_AUTHORS = ['Guest']
 # Final location for the main blog page and sibling paginated pages is
 # output / TRANSLATION[lang] / INDEX_PATH / index-*.html
 # (translatable)
-INDEX_PATH = 'blog'
+#  INDEX_PATH = ""
 
 # Optional HTML that displayed on “main” blog index.html files.
 # May be used for a greeting. (translatable)
