9f92e041dece89282d0fb692dac3679853d54ec4
Santos Gallegos <santos_g@outlook.com>
2018-10-30 22:47:57 -0500

    Agregar Google Analytics
    Está configurado con la cuenta de la comunidad
    
    ecuadorpython@gmail.com

--- conf.py
+++ conf.py
@@ -1257,7 +1257,17 @@ COPY_SOURCES = False
 # Extra things you want in the pages HEAD tag. This will be added right
 # before </head>
 # (translatable)
-# EXTRA_HEAD_DATA = ""
+EXTRA_HEAD_DATA = """
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-128414584-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-128414584-1');
+</script>
+"""
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
