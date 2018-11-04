0965c3671d8ed94d9fb106c497ec6cbbd0fd92f2
Santos Gallegos <santos_g@outlook.com>
2018-11-03 22:13:07 -0500

    Agregar index a guías

--- conf.py
+++ conf.py
@@ -145,6 +145,7 @@ NAVIGATION_LINKS = {
         ("/eventos/", "Eventos"),
         ("/reglas/", "Reglas"),
         ("/quiero-ayudar/", "Quiero ayudar"),
+        ("/guias/", "Guías"),
         ("/becas/", "Becas"),
         ("/sponsors/", "Sponsors"),
         ("/quienes-somos/", "¿Quiénes somos?"),
