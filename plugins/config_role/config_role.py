9c5941ca9fb13f73777a11d48a00fe9130e221c0
Santos Gallegos <santos_g@outlook.com>
2018-11-23 13:58:51 -0500

    Pasar valor de configuración por el parser de rst

--- plugins/config_role/config_role.py
+++ plugins/config_role/config_role.py
@@ -1,6 +1,6 @@
-from docutils import nodes
-from docutils import utils
-from docutils.parsers.rst import roles
+from docutils.frontend import OptionParser
+from docutils.parsers.rst import Parser, roles
+from docutils.utils import new_document
 from nikola.plugin_categories import RestExtension
 
 
@@ -9,15 +9,32 @@ class Plugin(RestExtension):
     name = 'config_role'
 
     def set_site(self, site):
-        config_role.site = site
-        roles.register_canonical_role('config', config_role)
+        roles.register_canonical_role('config', config_role(site))
         return super().set_site(site)
 
 
-def config_role(
-        name, rawtext, text, lineno, inliner, options=None, content=None):
-    options = options or {}
-    content = content or []
-    #  print(config_role.site.GLOBAL_CONTEXT)
-    config = config_role.site.config.get(text, '')
-    return [nodes.inline(rawtext, config)], []
+def config_role(site):
+    """
+    Inject a value from the ``conf.py`` file.
+
+    Before injecting the value, it is parsed by the rst parser,
+    that way we can have a link when reading an email from the config
+    for example.
+
+    This role needs access to the `site` object.
+    As we can't pass extra args to a role function we are using a closure.
+    """
+    def role(name, rawtext, text, lineno, inliner, options=None, content=None):
+        parser = Parser()
+        settings = OptionParser(components=(Parser,)).get_default_values()
+        document = new_document('Raw rst from config value', settings)
+
+        config_value = site.config.get(text, '')
+        parser.parse(config_value, document)
+
+        # The whole text is interpreted as a big paragraph
+        # causing a new line to be inserted at the end,
+        # so we only take the elements inside that paragraph.
+        paragraph, *_ = document.children
+        return paragraph.children, []
+    return role
