3bc31de73d5576bc0bf5090a04bb62b2a6e43203
Santos Gallegos <santos_g@outlook.com>
2018-11-23 14:15:22 -0500

    Documentar uso

--- plugins/config_role/config_role.py
+++ plugins/config_role/config_role.py
@@ -1,3 +1,15 @@
+"""
+Add the `config` role, it reads a value from the conf.py file.
+
+Usage:
+
+   Send us an email to :config:`BLOG_EMAIL`
+
+Here `BLOG_EMAIL` is the variable name from the conf.py file.
+Note that the text is rendered as an email link, not just as plain text.
+This is because the value is interpreted as reStructuredText.
+"""
+
 from docutils.frontend import OptionParser
 from docutils.parsers.rst import Parser, roles
 from docutils.utils import new_document
