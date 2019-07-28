bd5eb6146e1b01467b29cac96ce2f5d2859a3761
Santos Gallegos <santos_g@outlook.com>
2019-07-28 01:07:55 -0500

    Agregar sistema de comentarios

--- conf.py
+++ conf.py
@@ -1006,12 +1006,12 @@ RSS_COPYRIGHT_FORMATS = CONTENT_FOOTER_FORMATS
 # systems.  The following comment systems are supported by Nikola:
 #   disqus, facebook, intensedebate, isso, muut, commento
 # You can leave this option blank to disable comments.
-COMMENT_SYSTEM = ""
+COMMENT_SYSTEM = "disqus"
 # And you also need to add your COMMENT_SYSTEM_ID which
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = ""
+COMMENT_SYSTEM_ID = "pythonecuador"
 
 # Create index.html for page folders?
 # WARNING: if a page would conflict with the index file (usually
