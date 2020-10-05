a97f3dc4e6c6006901784c29744edf43b4135612
Mayank-11 <70258893+Mayank-11@users.noreply.github.com>
2020-10-05 21:01:01 +0530

    Create drink_water_notification.py
    This is a notification giver to remind things.

---
+++ drink_water_notification.py
@@ -0,0 +1,13 @@
+import time
+from plyer import *
+
+if __name__ == "__main__":
+    print("Started")
+    notification.notify(
+        title = "Drink Water!",
+        message = "1. Drinking Water Helps Maintain the Balance of "
+                  "Body Fluids. Your body is composed of about 60% water. ",
+        timeout = 10
+    )
+print("Hello World")
+print("Hey there")
