19020ca4e0a1b5469d73508fc92d5d410b0037f0
Israel Teneda <israteneda@gmail.com>
2021-10-13 21:02:49 -0500

    Añadir black y formatear archivos de python

--- Makefile
+++ Makefile
@@ -7,4 +7,10 @@ serve:
 deploy:
 	NIKOLA_DEPLOY=python.ec nikola github_deploy
 
+lint:
+	python -m nox -r -s lint_python
+
+format:
+	python -m nox -r -s format_python
+
 .PHONY: clean serve
--- noxfile.py
+++ noxfile.py
@@ -3,18 +3,30 @@ import nox
 
 @nox.session
 def tests(session):
-    session.install('-r', 'requirements.txt')
-    session.run('nikola', 'build', '--strict')
-    session.run('nikola', 'check', '--check-links', '--check-files')
+    session.install("-r", "requirements.txt")
+    session.run("nikola", "build", "--strict")
+    session.run("nikola", "check", "--check-links", "--check-files")
 
 
 @nox.session
 def lint_rst(session):
-    session.install('rstcheck')
-    session.run('rstcheck', '-r', '.')
+    session.install("rstcheck")
+    session.run("rstcheck", "-r", ".")
 
 
 @nox.session
 def lint_yaml(session):
-    session.install('yamllint')
-    session.run('yamllint', '-s', 'data')
+    session.install("yamllint")
+    session.run("yamllint", "-s", "data")
+
+
+@nox.session
+def lint_python(session):
+    session.install("black")
+    session.run("black", "--check", ".")
+
+
+@nox.session
+def format_python(session):
+    session.install("black")
+    session.run("black", ".")
